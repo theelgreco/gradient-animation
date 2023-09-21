ef97862a5c1b8df942a827741054bdbd793ffbf4
theelgreco <stelios.kakoliris@yahoo.com>
2023-09-21 13:20:55 +0100

    refactored update function

--- utils/animationFunctions.js
+++ utils/animationFunctions.js
@@ -27,6 +27,7 @@ function linearGradientStringToNestedArray(str) {
     }
   }
 
+  console.log(res);
   return res;
 }
 
@@ -60,8 +61,8 @@ function findDiffIndex(arr1, arr2) {
     const subIndex = parseFloat(arr1[i][1]) > -1 ? 1 : 0;
     diff.index = i;
     diff.subIndex = subIndex;
-    diff.from = parseFloat(arr1[i][subIndex]);
-    diff.to = parseFloat(arr2[i][subIndex]);
+    diff.startValue = parseFloat(arr1[i][subIndex]);
+    diff.endValue = parseFloat(arr2[i][subIndex]);
     diff.unit = arr1[i][1].match(unitRegex)[0];
     break;
   }
@@ -88,58 +89,103 @@ function formatStringFromArr(arr) {
   return resString;
 }
 
-function animate(element, keyframes, iterations) {
+function checkIfAnimationFinished(
+  element,
+  elapsedTime,
+  duration,
+  startTime,
+  toStrNestedArr,
+  currentStep,
+  currentIteration,
+  totalIterations,
+  totalSteps
+) {
+  // check if the keyframe has ended, a.k.a if the time passed is equal to or greater than the total duration of the keyframe
+  if (elapsedTime >= duration) {
+    element.style.background = formatStringFromArr(toStrNestedArr);
+    currentStep++;
+
+    // check if the current step (keyframe)
+    if (currentStep === totalSteps) {
+      currentStep = 0; // Reset to the first keyframe for the next iteration
+      currentIteration++;
+
+      if (totalIterations !== undefined && currentIteration > totalIterations) {
+        return; // All totalIterations completed
+      } else if (totalIterations === undefined) {
+        return; // Run only once if iterations is undefined
+      }
+    }
+
+    startTime = performance.now();
+  }
+}
+
+function animate(element, steps, iterations) {
   console.log("starting animation...");
   let currentIteration = 1;
   let startTime = performance.now();
-  const steps = keyframes.length;
+  const totalNumOfSteps = steps.length;
   let currentStep = 0;
 
   function update(currentTime) {
     console.log("animating...");
     const elapsedTime = currentTime - startTime;
-    const currentKeyframe = keyframes[currentStep];
+    const currentKeyframe = steps[currentStep];
 
-    // find difference
+    // Takes the from property and turns into a nested array, e.g:
+    // from: "linear-gradient(90deg, red, orange 50%, red)",
+    //    => [["90", "deg"], ["red"], ["orange", "50%"], ["red"]]
     const fromStrNestedArr = linearGradientStringToNestedArray(
       currentKeyframe.from
     );
+
+    // Takes the to property and turns into a nested array, e.g:
+    // from: "linear-gradient(90deg, red, orange 50%, red)",
+    //    => [["90", "deg"], ["red"], ["orange", "10%"], ["red"]]
     const toStrNestedArr = linearGradientStringToNestedArray(
       currentKeyframe.to
     );
-    const { index, subIndex, from, to, unit } = findDiffIndex(
+
+    const { duration } = steps[currentStep];
+    const method = timingFunctions[steps[currentStep].method];
+
+    // check if the animation has ended
+    checkIfAnimationFinished(
+      element,
+      elapsedTime,
+      duration,
+      startTime,
+      toStrNestedArr,
+      currentStep,
+      currentIteration,
+      iterations,
+      totalNumOfSteps
+    );
+
+    // takes the two nested arrays representing the from step and to step and finds the different value
+    const { index, subIndex, startValue, endValue, unit } = findDiffIndex(
       fromStrNestedArr,
       toStrNestedArr
     );
 
-    const { duration } = keyframes[currentStep];
-    const method = timingFunctions[keyframes[currentStep].method];
-
-    if (elapsedTime >= duration) {
-      element.style.background = formatStringFromArr(toStrNestedArr);
-      currentStep++;
-
-      if (currentStep === steps) {
-        currentStep = 0; // Reset to the first keyframe for the next iteration
-        currentIteration++;
-
-        if (iterations !== undefined && currentIteration > iterations) {
-          return; // All iterations completed
-        } else if (iterations === undefined) {
-          return; // Run only once if iterations is undefined
-        }
-      }
-
-      startTime = performance.now();
-    }
-
+    // progress is a percent of time passed,
+    // e.g if 1000ms has passed when duration is 2000ms then progress = 0.5 => 50%
     const progress = Math.min(elapsedTime / duration, 1);
+    // this takes the progress and passes it to the easing function which returns a new percentage based on the easing method
+    // e.g, if method is ease-in and progress = 0.5 => it may return 0.35 (not accurate just an example)
     const easedProgress = method(progress);
-    const currentValue = from + (to - from) * easedProgress;
 
+    // totalChange is equal to the total amount the value changes from one frame to the next,
+    // e.g if start = 50 & end = 100 => totalChange = 50
+    const totalChange = endValue - startValue;
+    const currentValue = (startValue + totalChange) * easedProgress;
+
+    // update the value in the nested array, if it has
     fromStrNestedArr[index][subIndex] =
       subIndex === 0 ? `${currentValue}` : `${currentValue}${unit}`;
 
+    // takes the nested array and turns it into a valid CSS string and applies it as the background
     element.style.background = formatStringFromArr(fromStrNestedArr);
 
     requestAnimationFrame(update);
